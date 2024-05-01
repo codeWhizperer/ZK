1115a9e017e9d5aaf8675dda6229d3cf1e03cbd8
Adegbite Ademola Kelvin <adegbiteademolakelvin@Adegbites-MacBook-Pro.local>
2024-05-01 23:27:36 +0100

    update

--- polynomial/src/main.rs
+++ polynomial/src/main.rs
@@ -7,10 +7,23 @@ fn main() {
     #[generator = "3"]
     pub struct FqConfig;
     pub type Fq = Fp64<MontBackend<FqConfig, 1>>;
-    let coeffs = vec![Fq::from(1), Fq::from(2), Fq::from(5)];
+    let coeffs = vec![Fq::from(1), Fq::from(16), Fq::from(13)];
     let new_polynomial = UnivariatePolynomial::new(coeffs);
     let evaluate_result = UnivariatePolynomial::evaluate(&new_polynomial,&Fq::from(2));
     println!("{}", new_polynomial);
     println!("{}", evaluate_result);
+
+// a = [0, 3, 2]
+// b = [9, 0, 2, 3]
+    let coef_one = { vec![Fq::from(0), Fq::from(3), Fq::from(2)] };
+    let coef_two = {  vec![Fq::from(9), Fq::from(0), Fq::from(2), Fq::from(3)] };
+    let polynomial_one = UnivariatePolynomial::new(coef_one);
+    let polynomial_two = UnivariatePolynomial::new(coef_two);
+    let poly_res = polynomial_one + polynomial_two;
+    let res_coeffs = vec![Fq::from(1), Fq::from(3), Fq::from(2)];
+    let res_coeffs2 =  {  vec![Fq::from(5), Fq::from(0), Fq::from(2), Fq::from(4)] };
+    println!("{}", poly_res);
+    println!("{:?}", res_coeffs);
+    println!("{:?}", res_coeffs2);
 
 }
--- polynomial/src/univariate_polynomial/mod.rs
+++ polynomial/src/univariate_polynomial/mod.rs
@@ -1,8 +1,9 @@
 // given the list [2,3,4] transform to -> (0,2) + (1,3) + (2,4) -> 2 + 3x + 4x;
 
 use ark_ff::PrimeField;
+use std::ops::{Add,Mul,Div};
 use core::fmt;
-#[derive(Debug)]
+#[derive(Debug,Clone)]
 pub struct UnivariatePolynomial<F:PrimeField> {
     coef: Vec<F>,
 }
@@ -33,7 +34,88 @@ impl<F:PrimeField> UnivariatePolynomial<F> {
         self.coef.iter().rev().fold(F::zero(), |acc, cof| acc * x + cof)
     }
 
+    pub fn interpolate(x_values: Vec<F>, y_values: Vec<F>) -> Self {
+        assert_eq!(x_values.len(), y_values.len());
+        let mut result = Self::new(vec![]);
+        for i in 0..x_values.len() {
+            let mut y_val_polynomial = Self::new(vec![y_values[i]]);
+            for j in 0..x_values.len() {
+                if i == j {
+                    continue;
+                }
+                let numerator = Self::new(vec![-x_values[j], F::one()]);
+                let mut denominator = Self::new(vec![F::one()]);
+                for k in 0..x_values.len() {
+                    if k == i || k == j {
+                        continue;
+                    }
+                    denominator = denominator * Self::new(vec![-x_values[k], F::one()]);
+                }
+                y_val_polynomial = y_val_polynomial * numerator * denominator.reciprocal();
+            }
+            result = result +  y_val_polynomial;
+        }
+        result
+    }
+    
+
+    
 
 }
 
+// a = [0, 3, 2]
+// b = [9, 0, 2, 3]
 
+impl<F:PrimeField> Mul for UnivariatePolynomial<F>
+where
+    F: Copy + Mul<Output = F> + Default,
+{
+    type Output = Self;
+
+    fn mul(self, other: Self) -> Self::Output {
+
+        let mut result_coefficients = vec![F::default(); self.coef.len() + other.coef.len() - 1];
+        for (i, &coeff1) in self.coef.iter().enumerate() {
+            for (j, &coeff2) in other.coef.iter().enumerate() {
+                result_coefficients[i + j] = result_coefficients[i + j] + coeff1 * coeff2;
+            }
+        }
+        Self {
+            coef: result_coefficients,
+        }
+    }
+}
+
+impl<F:PrimeField> Add for UnivariatePolynomial<F>
+where
+    F: Copy + Add<Output = F> + Default,
+{
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self::Output {
+        let max_degree = self.coef.len().max(other.coef.len());
+        let mut result_coefficients = vec![F::default(); max_degree];
+        for i in 0..max_degree {
+            let coeff1 = if i < self.coef.len() { self.coef[i] } else { F::default() };
+            let coeff2 = if i < other.coef.len() { other.coef[i] } else { F::default() };
+            result_coefficients[i] = coeff1 + coeff2;
+        }
+        Self {
+            coef: result_coefficients,
+        }
+    }
+}
+
+
+impl<F:PrimeField> UnivariatePolynomial<F>
+where
+    F: Copy + Default + Div<Output = F> + PartialEq + Mul<Output = F>,
+{
+    fn reciprocal(&self) -> Self {
+        let mut result = self.clone();
+        for coeff in &mut result.coef {
+            *coeff = F::one() / *coeff;
+        }
+        result
+    }
+}
